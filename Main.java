1745790cb86ebedf962204fc47680abd46c8f1d0
VIDO VILLALVAZO ENRIQUE <zS23017366@estudiantes.uv.mx>
2024-08-26 12:25:36 -0600

    Main

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      // Scanner sc = new Scanner(System.in);
+      // int n = sc.nextInt();
+
+      // if (n == 100) {
+      //   System.out.println("N es correcto");
+      // } else{
+      //   System.out.println("N es incorrecto");
+      // }
+
+      
+
+      // System.out.println("(19.45 * 4421) / 2) = ");
+      // System.out.print((19.45 * 4421) / 2);
+
+      // for(int i = 1; i <= 10; i ++){
+      //   System.out.println(i);
+      // }
+      
+      // int n = 1; 
+      // while (n < 10) {
+      //   System.out.println("Migueeel");
+      //   n++;
+      // }
+
+      // int contador = 1; 
+
+      // Scanner sc = new Scanner(System.in);
+      // String nombre = sc.next();
+      
+      // do {
+      //   System.out.println(nombre);
+      //   contador ++;
+      // }
+      // while (contador < 10);
+    }
+}
