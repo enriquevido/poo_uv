6f26c7d4f6180043b1d596259b452b42771a4001
Enrique Vidó Villalvazo <enriquevido@MacBook-Air-de-Enrique.local>
2024-09-30 22:51:35 -0600

    TV

---
+++ 30-09/src/TV.java
@@ -0,0 +1,48 @@
+public class TV {
+    private int channel;
+    private int volume;
+    private boolean on;
+
+    TV(){
+        channel = 1;
+        volume = 0;
+        on = false;
+    }
+
+    public void turnOn(){
+        this.on = true;
+        System.out.println("TV ON");
+    }
+
+    public void turnOff(){
+        this.on = false;
+        System.out.println("TV OFF");
+    }
+
+    public void setChannel(int channel){
+        this.channel = channel;
+        System.out.println("Channel = " + channel);
+    }
+
+    public void setVolume(int volume){
+        this.volume = volume;
+        System.out.println("Volume = " + volume);
+    }
+
+    public void channelUp(){
+        channel++;
+    }
+
+    public void channelDown(){
+        channel--;
+    }
+
+    public void volumeUp(){
+        volume++;
+    }
+
+    public void volumeDown(){
+        volume--;
+    }
+
+}
