61c0ce9549421b9411e994b8ef5c50b85dccd89d
Enrique Vidó Villalvazo <enriqu_vido23@outlook.com>
2024-10-22 13:36:55 -0600

    Test File Stream

---
+++ TestFileStream/src/TestFileStream.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class TestFileStream {
+    public static void main(String[] args) throws IOException {
+        try (
+            FileOutputStream output = new FileOutputStream("temp.dat");
+        ) {
+            for (int i = 0; i <= 10; i++) {
+                output.write(i);
+            }
+        }
+        try (
+            FileInputStream input = new FileInputStream("temp.dat");
+        ) {
+            int value;
+            while ((value = input.read()) != -1) {
+                System.out.print(value + " ");
+            }
+        }
+    }
+}
